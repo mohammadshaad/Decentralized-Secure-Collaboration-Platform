e8b4c7a4372aea02a02e208d2d7d41f4a2909a34
Adarsh Gaurav <100284189+Adarsh0503@users.noreply.github.com>
2024-03-08 10:41:11 +0530

    Happy Happy

--- @/components/Hero.tsx
+++ @/components/Hero.tsx
@@ -1,9 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { Button } from "./ui/moving-border";
+
+import { Spotlight } from "./ui/Spotlight";
 
 function Hero() {
   return (
+
+    
     <div className='container flex flex-col items-start justify-center w-full  max-h-screen h-[500px]'>
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="pink"
+      />
       <div className='text-4xl md:text-7xl text-left md:text-center font-extrabold md:max-w-6xl leading-tight uppercase'>
         <span className='hero-text'>
           Descentralised
@@ -19,8 +28,8 @@ function Hero() {
       </div>
 
       <div className='w-full flex items-center justify-center mt-5'>
-        <Link href="/chat" className='submit-button hover:border-white text-center dark:text-white dark:hover:border-black border border-[#e536ab] text-black transition-all duration-300 rounded-full hover:text-white text-xs font-light w-full md:w-auto py-3 px-7 mt-5 uppercase ring-0 outline-none'>
-          Get Started
+        <Link href="/chat" className='submit-button hover:border-white text-center dark:text-white dark:hover:border-black border border-[#e536ab] text-black transition-all duration-300 rounded-full hover:text-white text-xs font-light w-full md:w-auto py-2 px-2 mt-5 uppercase ring-0 outline-none'>
+          <Button> Get Started</Button>
         </Link>
       </div>
 
